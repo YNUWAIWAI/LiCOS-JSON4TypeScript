707d59a668a5fc302aa9aa850deb018f23755102
Yudai Udagawa <nimius.rd@gmail.com>
2019-09-13 05:54:50 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-interface I {
-  a: string
-}
+import {global, indexedDB, socket} from 'client/village/constants/ActionTypes'
+import {createStore} from 'redux'
+import middleware from './middlewares'
+import reducer from './reducers'
 
-const obj: I = {
-  a: 'a'
-}
-console.log(obj)
+const store = createStore(
+  reducer,
+  middleware
+)
+
+store.dispatch({
+  type: socket.INIT
+})
+store.dispatch({
+  type: global.PROLOGUE
+})
