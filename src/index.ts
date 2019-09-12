b4b9bd23e8c691c58add42e2fca483a7d0891132
Yudai Udagawa <nimius.rd@gmail.com>
2019-09-13 06:05:36 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import {global, indexedDB, socket} from 'client/village/constants/ActionTypes'
+import * as village from 'types/village'
+import {global, socket} from 'client/village/constants/ActionTypes'
 import {createStore} from 'redux'
+import {postChat, selectOption} from 'client/village/actions'
 import middleware from './middlewares'
 import reducer from './reducers'
 
@@ -14,3 +16,32 @@ store.dispatch({
 store.dispatch({
   type: global.PROLOGUE
 })
+
+export const sendChat = (text: string) => {
+  store.dispatch(
+    postChat({
+      channel: village.InputChannel.public,
+      text
+    })
+  )
+}
+export const hunterVote = (id: village.AgentId) => {
+  store.dispatch(
+    selectOption(id)
+  )
+}
+export const noonVote = (id: village.AgentId) => {
+  store.dispatch(
+    selectOption(id)
+  )
+}
+export const seerVote = (id: village.AgentId) => {
+  store.dispatch(
+    selectOption(id)
+  )
+}
+export const werewolfVote = (id: village.AgentId) => {
+  store.dispatch(
+    selectOption(id)
+  )
+}
