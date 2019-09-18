fd6965b5a9e1db07cd993d593cd71a11a5163cb7
Yudai Udagawa <nimius.rd@gmail.com>
2019-09-19 00:41:42 +0900

    Fix config

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,3 +1,22 @@
+import {connect} from '../src'
+
 test('test', () => {
+  const socket = connect({
+    hunterVote: (send, json, state) => {
+      send('0')
+    },
+    noonVote: (send, json, state) => {
+      send('0')
+    },
+    seerVote: (send, json, state) => {
+      send('0')
+    },
+    sendChat: (send, json, state) => {
+      send('text')
+    },
+    werewolfVote: (send, json, state) => {
+      send('0')
+    }
+  })
   expect(1).toBe(1)
 })
